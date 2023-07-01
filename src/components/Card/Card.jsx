702bc140bc48c563a4cc488429469d33c3014bbb
Ankita297 <ankitapal7777@gmail.com>
2023-07-01 12:30:52 +0530

    added a loading-spinner svg

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import LoadingSpinner from "../../asset/LoaderSpinner.svg";
 const Card = ({ img, type, title }) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <div>
       <p>{title}</p>
-      <img src={img} alt={title} />
+      {loading && <img src={LoadingSpinner} />}
+      <img src={img} alt={title} onLoad={handleLoad} />
     </div>
   );
 };
